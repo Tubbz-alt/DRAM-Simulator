7139e4d1880aa4f77c8423e414bea07ad8137cd6
Pablo <pablo.riutort@gmail.com>
2017-02-17 18:34:18 +0100

    Make create_file() more random
    Add random functionality to create_file().
    Now reads a memory dump backup file from simulator and chooses one
    random line.
    Writes the random line in the memory_content.txt file and thus the
    random behaviour

--- daemon.py
+++ daemon.py
@@ -3,12 +3,25 @@
 from os.path import exists
 from os import remove
 from time import sleep
+from random import choice
 
 
 def create_file():
+    print("Read random line from backup file")
+    random_line = ""
+    with open('backup_memory.txt', 'r') as memory:
+        # read line by line the given file and split the
+        # line in order to get the words
+        aux = choice([line.split() for line in memory])
+        # pass the elements of the list to the line
+        for a in aux:
+            random_line += a+" "
+        random_line+"\n"
+    memory.close
+            
     print("Creating memory file")
     with open(filename, 'w') as f:
-        f.write("r 536980608 51989\n")
+        f.write(random_line)
     f.close
 
 filename = 'memory_content.txt'
@@ -24,4 +37,4 @@ while True:
         create_file()
     else:
         # bootstrap the thing
-        create_file()        
+        create_file()
