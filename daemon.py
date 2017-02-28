99d2352544599b371ecd68b4ebf782bbb7d1bcc6
Pablo <pablo.riutort@gmail.com>
2017-02-28 21:24:50 +0100

    Add HALT into memory when exiting terminal
    When the KeyboardInterrupt exception is raised the program will write a
    HALT string into the memory file, thus emulating more precisely the
    behaviour of the true simulator

--- daemon.py
+++ daemon.py
@@ -8,33 +8,41 @@ from random import choice
 
 filename = 'memory_content.txt'
 
-while True:
-    while exists(filename):
-        print("Wait for it...")
-        sleep(1)
+
+def create_memory(filename,content):
+    with open(filename, 'w') as f:
+        f.write(content)
+    f.close
+    
+
+try:
+    while True:
+        while exists(filename):
+            print("Wait for it...")
+            sleep(1)
+            
+        print("Read random line from backup file")
+        random_line = ""
+        with open('backup_memory.txt', 'r') as memory:
+            # read line by line the given file and split the
+            # line in order to get the words
+            aux = choice([line.split() for line in memory])
+            # pass the elements of the list to the line
+            for a in aux:
+                random_line += a+" "
+            random_line+"\n"
+        memory.close
         
-    print("Read random line from backup file")
-    random_line = ""
-    with open('backup_memory.txt', 'r') as memory:
-        # read line by line the given file and split the
-        # line in order to get the words
-        aux = choice([line.split() for line in memory])
-        # pass the elements of the list to the line
-        for a in aux:
-            random_line += a+" "
-        random_line+"\n"
-    memory.close
-    
-    if not exists(filename):
-        print("Creating memory file")
-        with open(filename, 'w') as f:
-            f.write(random_line)
-        f.close
-    
-    # signal
-    if exists('signal'):
-        with open('signal', 'r') as f:
-            print("Total wait it",f.read())
-        f.close
-        print("Deleting signal file")
-        remove('signal')
+        if not exists(filename):
+            print("Creating memory file")
+            create_memory(filename,random_line)
+        
+        # signal
+        if exists('signal'):
+            with open('signal', 'r') as f:
+                print("Total wait it",f.read())
+            f.close
+            print("Deleting signal file")
+            remove('signal')
+except KeyboardInterrupt:
+    create_memory(filename,"HALT\n")
