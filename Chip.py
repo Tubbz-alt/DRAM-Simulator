03170d8805c28732b9de43a7997081dd175bc069
Pablo <pablo.riutort@gmail.com>
2017-05-19 04:08:52 +0200

    Trim Chip
    Remove useless functions never used (setters and getters)
    Remove default param values

--- Chip.py
+++ Chip.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 class Chip(object):
-    def __init__(self, capacity=0, rows=2, columns=2, banks=1):
+    def __init__(self, capacity, rows, columns, banks):
         self.__capacity = positive(capacity)
         self.__rows = positive(pair(rows))
         self.__columns = positive(pair(columns))
@@ -22,42 +22,9 @@ class Chip(object):
 
 
     @property
-    def capacity(self):
-        return self.__capacity
-
-
-    @capacity.setter
-    def capacity(self, value):
-        self.__capacity = positive(value)
-
-    @property
-    def rows(self):
-        doc = "The rows."
-        return self.__rows
-
-    @rows.setter
-    def rows(self, value):
-        self.__rows = positive(pair(value))
-
-
-    @property
-    def columns(self):
-        doc = "The columns."
-        return self.__columns
-
-    @columns.setter
-    def columns(self, value):
-        self.__columns = positive(pair(value))
-
-
-    @property
     def banks(self):
-        doc = "The banks property."
         return self.__banks
 
-    @banks.setter
-    def banks(self, value):
-        self.__banks = positive(value)
 
     def update(self,bank,row):
         self.__banks = [[False for i in range(self.__rows)] for i in range(self.__num_banks)]
@@ -74,5 +41,4 @@ def pair(value):
     if value % 2 != 0:
         raise ValueError("Value must be pair")
     return value
-
 
